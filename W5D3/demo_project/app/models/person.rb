74417c0fa3e5564a4f41fe9359cef5392c738671
Charrs-Barwin <mikehollander84@gmail.com>
2022-04-28 01:04:47 -0400

    fix

--- W5D3/demo_project/app/models/person.rb
+++ W5D3/demo_project/app/models/person.rb
@@ -4,7 +4,7 @@ class Person < ApplicationRecord
 
     # belongs_to(:house, {
     #     primary_key: :id,
-    #     foreign_key: :house_id,
+    #     foreign_key: :X_id,
     #     class_name: :House
     # })
 end
